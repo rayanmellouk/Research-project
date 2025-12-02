f6e572ad41b5ad92c251ffe34a9a637e60536a06
Raphaël Herbeaux <raphael.herbeaux@gmail.com>
2025-12-02 13:30:16 +0100

    Début RL

---
+++ DDRL/agents/ddrl_agent.py
@@ -0,0 +1,14 @@
+def compute_action(self, state):
+    return
+
+def update_policy(self, batch):
+    return
+
+def save_model(self, filepath):
+    return
+
+def load_model(self, filepath):
+    return 
+
+#contient le NN
+#différentiation automatique
